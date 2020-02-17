f7a9ae90bce34d7532b7a778921086089e4a6c9a
cyt <470922684@qq.com>
2020-02-18 00:27:43 +0800

    图表（没适配）

---
+++ js/index.js
@@ -0,0 +1,30 @@
+// bat3_pie = echarts.init(document.getElementById("bat3_pie"));
+// bat3_pie.setOption({
+//     color: ["#fe0100", "#00ff36", "#3399ff"],
+//     tooltip: {
+//         trigger: "item",
+//         formatter: "{a} <br/>{b} : {c} ({d}%)"
+//     },
+//     series: [{
+//         name: "电量",
+//         type: "pie",
+//         radius: "55%",
+//         center: ["50%", "50%"],
+//         label: {
+//             color: "#fff"
+//         },
+//         // startAngle: -20,  // 起始角度
+//         data: [
+//             { value: 30, name: "可放电量" },
+//             { value: 70, name: "可充电量" }
+//         ],
+//         emphasis: {
+//             itemStyle: {
+//                 shadowBlur: 10,
+//                 shadowOffsetX: 0,
+//                 shadowColor: "rgba(0, 0, 0, 0.5)"
+//             }
+//         }
+//     }]
+// });
+// bat3_pie.resize();
